82b28586e052bfc8490daff28a5309c1d78f57f1
¨jumartinezfer¨ <jumartinezfer95@gmail.com>
2025-03-21 13:50:33 -0500

    con actividad 3

--- src/middlewares/logger-middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoggerMiddleware } from './logger-middleware'
-
-describe('LoggerMiddlewareMiddleware', () => {
-  it('should be defined', () => {
-    expect(new LoggerMiddleware()).toBeDefined()
-  })
-})
